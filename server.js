ea275db73ffd5716521a5918bce3eb38e4ed3085
Your Name <you@example.com>
2023-01-11 14:40:58 +0000

    changes

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const io = require("socket.io")(server, {
   pingTimeout: 60000,
   // cors to stop any cross orgin issues
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://chatify-b5yp.onrender.com"],
     credentials: true,
   },
 });
